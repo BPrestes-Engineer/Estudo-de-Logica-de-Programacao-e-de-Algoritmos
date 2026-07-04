c205165e89d2c74a613168899eaf7a555f2b6111
BPrestes <prestesbruno378@gmail.com>
2026-07-03 22:05:44 -0300

    update

---
+++ C++/Olá, Mundo!!! ^U^.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+/*
+Iostream é uma biblioteca
+I - vem de Input (Entrada)
+O - vem de Output (Saída)
+*/
+
+#include <locale.h> //Para usar acento: ´´``^^~~...
+
+using namespace std;
+
+/*
+Importando todos os identificadores do NameSpace STD para usar o cout e endl
+O Namespace é uma organização de pastas, nada além disso.
+*/
+
+int main(){
+	
+/*
+Todo o programa em C++ tem uma e somente uma função principal que é a função "MAIN"
+*/
+	
+	setlocale(LC_ALL, "");
+	
+	cout << "\nOlá, Mundo!!! ^U^ \n" << endl;
+
+	cout << "Hello, WORLD!!! \n I'm BPrestes-Engineer (^^)" << endl;
+
+	//A função main diz que retornar um inteiro, então colocamos o 0 como um tipo de retorno
+	;return 0;	
+	
+}
